9cd5bdb9ff6751d01e849777b10f11d2bb3404a8
YASH RAJ <yrajgecarwl@gmail.com>
2025-11-12 00:08:03 +0530

    Add Hello World route to app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,11 +6,12 @@ const app = express()
 // Middleware to serve static files from the 'public' directory
  
 app.use(cors());
-// app.get('/', (req, res) => {
-//     res.send('Hello World!')
-// })
+ app.get('/', (req, res) => {
+     res.send('Hello World!')
+ })
 
 app.use(express.json());
 
 app.use('/ai' , aiRoutes);
+
 module.exports = app
